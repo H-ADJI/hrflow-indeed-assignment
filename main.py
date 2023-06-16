13db2e4ec1beef1b30f1b2aad9a1ce00499b10bc
h-adji <h-adji_tech@proton.me>
2023-06-16 11:32:13 +0100

    added url and title and other extractions

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from playwright_stealth import stealth_sync
 from navigation import paginate
 from parsing import extract_initial_info
 with sync_playwright() as p:
-    browser: Browser = p.chromium.launch(headless=False)
+    browser: Browser = p.chromium.launch(headless=True)
     context = browser.new_context(
         viewport={"width": 1920, "height": 1080},
         user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",  # noqa
