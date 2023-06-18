5bbf5e0b4dccc9b3e4790b464cce7fc923e3b2aa
h-adji <h-adji_tech@proton.me>
2023-06-18 22:57:25 +0100

    added adapter from raw data to hrflow data format

--- src/data_models.py
+++ src/data_models.py
@@ -3,8 +3,110 @@ from dataclasses import dataclass
 from datetime import datetime
 from typing import Literal, Optional
 
+from hrflow import Hrflow
+
 from src.constants import INDEED_BASE_URL
 from src.utils import nested_get
+
+
+@dataclass
+class Location:
+    text: str
+    lat: float = None
+    lng: float = None
+
+
+@dataclass
+class Section:
+    name: str
+    title: str
+    description: str
+
+
+@dataclass
+class Skill:
+    name: str
+    type: Literal["hard", "soft"]
+    value: str = None
+
+
+@dataclass
+class DateRange:
+    name: str
+    value_min: datetime = None
+    value_max: datetime = None
+
+
+@dataclass
+class FloatRange:
+    name: str
+    value_min: float = None
+    value_max: float = None
+    unit: str = None
+
+
+@dataclass
+class Tag:
+    name: str
+    value: str
+
+
+@dataclass
+class Language:
+    name: str
+    value: str
+
+
+@dataclass
+class Task:
+    name: str
+    value: str
+
+
+@dataclass
+class Certification:
+    name: str
+    value: str
+
+
+@dataclass
+class Course:
+    name: str
+    value: str
+
+
+@dataclass
+class Metadata:
+    name: str
+    value: str
+
+
+@dataclass
+class HrflowJob:
+    """
+    The Hrflow job model, this is the format used to index jobs into hrflow
+    """
+
+    reference: str
+    name: str
+    location: Location
+    sections: list[Section]
+    url: str = None
+    summary: str = None
+    created_at: datetime = None
+    tags: list[Tag] = None
+    skills: list[Skill] = None
+    tasks: list[Task] = None
+    languages: list[Language] = None
+    certifications: list[Certification] = None
+    courses: list[Course] = None
+    ranges_float: list[FloatRange] = None
+    ranges_date: list[DateRange] = None
+    metadata: list[Metadata] = None
+
+    def add_hrflowAI_generated_field(self, text: str, client: Hrflow):
+        # here logic of including AI/NLP generated fields
+        pass
 
 
 @dataclass
@@ -25,7 +127,7 @@ class RawJob:
     company_logo: Optional[str] = None
     company_indeed_profile: Optional[str] = None
     company_review_page: Optional[str] = None
-    location: Optional[str] = None
+    job_location: Optional[str] = None
     raw_salary: Optional[str] = None
     salary_details: Optional[dict] = None
     date_posted: Optional[datetime] = None
@@ -35,8 +137,38 @@ class RawJob:
     shift: Optional[str] = None
 
     @property
-    def work_mode(self) -> list[str]:
-        return self.location
+    def clean_description(self) -> str:
+        if self.description:
+            return re.sub(
+                r"\n\s*\n",
+                "\n\n",
+                self.description.strip(),
+            )
+
+    @property
+    def full_description(self):
+        return self.title + "\n" + self.clean_description
+
+    @property
+    def work_modes(self) -> list[str]:
+        work_modes = []
+        if "remote" in self.job_location.lower() or "remote" in self.company_location.lower():
+            work_modes.append("remote")
+
+        if "hybrid" in self.company_location.lower():
+            work_modes.append("hybrid")
+
+        return work_modes
+
+    @property
+    def location(self) -> str:
+        compiled_regex = re.compile(r"((remote|hybrid)(\sin)?)")
+        compiled_regex.sub()
+        if job_loc := self.job_location:
+            from_job_location = compiled_regex.sub("", job_loc.lower())
+        if company_loc := self.company_location:
+            from_company_location = compiled_regex.sub("", company_loc.lower())
+        return from_company_location or from_job_location or None
 
     @property
     def full_url(self) -> str:
@@ -47,7 +179,7 @@ class RawJob:
             if raw_description := job_details.get("description"):
                 self.description = re.sub("<[^>]*>", "", raw_description)
             if location := nested_get(job_details, query="jobLocation.address.addressLocality"):
-                self.location = location
+                self.job_location = location
             salary_details: dict
             if salary_details := job_details.get("baseSalary"):
                 details: dict = salary_details.get("value")
@@ -84,69 +216,92 @@ class RawJob:
             if job_benefits := nested_get(job_metadata, query="benefitsModel.labels"):
                 self.job_benefits = job_benefits
 
+    def __location_adapter(self) -> Location:
+        return Location(text=self.location)
 
-@dataclass
-class Location:
-    text: str
-    lat: float = None
-    lng: float = None
+    def __tags_adapter(self) -> list[Tag]:
+        tags = []
+        for benefit in self.job_benefits:
+            tags.append[Tag(name="job benefit", value=benefit)]
+        for job_type in self.job_type:
+            tags.append(Tag(name="job type", value=job_type))
+        for work_mode in self.work_modes:
+            tags.append(Tag(name="work mode", value=work_mode))
+        if shift := self.shift:
+            tags.append(Tag(name="shift", value=shift))
+        if salary := self.raw_salary:
+            tags.append(Tag(name="raw salary", value=salary))
+        return tags or None
 
+    def __section_adapter(self) -> list[Section]:
+        return [
+            Section(
+                name="description", title="job description text", description=self.clean_description
+            )
+        ]
 
-@dataclass
-class Section:
-    name: str
-    title: str
-    description: str
+    def __float_ranges_adapter(self) -> list[FloatRange]:
+        ranges = []
+        if salary_details := self.salary_details:
+            salary_max = salary_details.get("maxValue")
+            salary_min = salary_details.get("minValue")
+            timeframe = salary_details.get("unitText")
+            currency = salary_details.get("currency")
+            if salary_min and salary_max and currency and timeframe:
+                ranges.append(
+                    FloatRange(
+                        name="salary",
+                        value_max=salary_max,
+                        value_min=salary_min,
+                        unit=f"{currency}/{timeframe}",
+                    )
+                )
+        return ranges or None
 
+    def __date_ranges_adapter(self) -> list[DateRange]:
+        return [
+            DateRange(name="job validity", value_max=self.date_posted, value_min=self.valid_until)
+        ]
 
-@dataclass
-class Skill:
-    name: str
-    type: Literal["hard", "soft"]
-    value: str = None
+    def __metadata_adapter(self) -> list[Metadata]:
+        metadatas = []
+        if company_name := self.company_name:
+            metadatas.append(Metadata(name="company name", value=company_name))
 
+        if company_rating := self.company_rating:
+            metadatas.append(Metadata(name="company rating", value=company_rating))
 
-@dataclass
-class DateRange:
-    name: str
-    value_min: datetime
-    value_max: datetime
+        if company_description := self.company_description:
+            metadatas.append(Metadata(name="company description", value=company_description))
 
+        if company_indeed_profile := self.company_indeed_profile:
+            metadatas.append(Metadata(name="company indeed profile", value=company_indeed_profile))
 
-@dataclass
-class FloatRange:
-    name: str
-    value_min: float
-    value_max: float
-    unit: str
+        if company_rating_count := self.company_rating_count:
+            metadatas.append(Metadata(name="company rating count", value=company_rating_count))
 
+        if company_review_page := self.company_review_page:
+            metadatas.append(Metadata(name="company review page", value=company_review_page))
 
-@dataclass
-class Tag:
-    name: str
-    value: str
+        if company_logo := self.company_logo:
+            metadatas.append(Metadata(name="company logo", value=company_logo))
 
+        return metadatas or None
 
-@dataclass
-class Metadata:
-    name: str
-    value: str
+    def gen_api_format(self, client: Hrflow) -> HrflowJob:
+        hrflow_job = HrflowJob(
+            reference=self.in_platform_id,
+            name=self.title,
+            location=self.__location_adapter(),
+            sections=self.__section_adapter(),
+            url=self.full_url,
+            created_at=self.date_posted,
+            tags=self.__tags_adapter(),
+            ranges_float=self.__float_ranges_adapter(),
+            ranges_date=self.__date_ranges_adapter(),
+            metadata=self.__metadata_adapter(),
+        )
+        # include NLP/AI generated fields
+        hrflow_job.add_hrflowAI_generated_field(text=self.full_description, client=client)
 
-
-@dataclass
-class HrflowJobWrite:
-    """
-    The Hrflow job model, this will be used to index jobs into hrflow
-    """
-    reference: str
-    name: str
-    location: Location
-    sections: list[Section]
-    url: str = None
-    summary: str = None
-    created_at: datetime = None
-    tags: list[Tag] = None
-    skills: list[Skill] = None
-    ranges_float: list[FloatRange] = None
-    ranges_date: list[DateRange] = None
-    metadata: list[Metadata] = None
+        return hrflow_job
