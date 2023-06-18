65bb34e90254a0d95d6027adda4856e15a3e96d2
h-adji <h-adji_tech@proton.me>
2023-06-18 22:57:47 +0100

    loading .env vars

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,20 @@
+from dataclasses import dataclass
+
+from dotenv import dotenv_values
+
+__CONFIG = dotenv_values(".env")
+
+
+@dataclass
+class EnvSettings:
+    API_KEY: str
+    USER_EMAIL: str
+    BOARD_KEY: str
+
+
+env_settings = EnvSettings(**__CONFIG)
+
+
 def nested_get(dictionary: dict, query: str):
     keys = query.split(".")
     for key in keys:
