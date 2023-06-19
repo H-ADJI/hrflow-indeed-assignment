1da8435a08d4c0f00da7b16f77a35b01c8c87aab
h-adji <h-adji_tech@proton.me>
2023-06-19 13:50:52 +0100

    testing the idnexing feature

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# //div[@class='icl-Autocomplete-ariaResultsAvailableWrapper']/span list for locations list
-# //input[@id='text-input-where'] input to click for locations
-# //ul[@class='icl-Autocomplete-list ']/li//b
-# //input DesktopSERPJobAlertPopup-email
 
-import json
-from dataclasses import asdict
-
+from hrflow import Hrflow
 from playwright.sync_api import Browser, sync_playwright
 from playwright_stealth import stealth_sync
 
+from src.indexing import index_job
 from src.navigation import feed_pagination, visit_job_page
 from src.parsing import extract_details, extract_initial_info
+from src.utils import env_settings
+hrflow_client = Hrflow(api_secret=env_settings.API_KEY, api_user=env_settings.USER_EMAIL)
 
-# import sys
-# logger.remove()
-# logger.add(sys.stderr, level="WARNING")
 with sync_playwright() as p:
     browser: Browser = p.chromium.launch(headless=True)
     context = browser.new_context(
@@ -33,15 +27,14 @@ with sync_playwright() as p:
     try:
         for page_content in feed_pagination(page=page):
             for job_info in extract_initial_info(indeed_feed_page=page_content):
-
                 if job_page_content := visit_job_page(page=details_page, job=job_info):
                     job_details, job_metadata = extract_details(page_content=job_page_content)
                     job_info.update_data(job_details=job_details, job_metadata=job_metadata)
-                    job = job_info.api_format(client=None)
-                    data.append(asdict(job))
+                    index_job(client=hrflow_client, extracted_job=job_info)
+
     finally:
-        with open("./assets/jobdata.json", "w") as f:
-            json.dump(data, f, ensure_ascii=False)
+
         details_page.screenshot(path="./assets/bugetails.jpg")
         page.screenshot(path="./assets/bugfeed.jpg")
+        page.pause()
     browser.close()
