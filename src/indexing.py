9878d1b8df77b828090a7dc370c29eda09481ea1
h-adji <h-adji_tech@proton.me>
2023-06-19 13:49:24 +0100

    added some logging to see what is getting indexed

--- src/indexing.py
+++ src/indexing.py
@@ -1,18 +1,23 @@
 from dataclasses import asdict
 
 from hrflow import Hrflow
+from loguru import logger
 
 from src.data_models import HrflowJob, RawJob
 from src.utils import env_settings
 
-hrflow_client = Hrflow(api_secret=env_settings.API_KEY, api_user=env_settings.USER_EMAIL)
 
-
-def index_job(client: Hrflow, raw_job: RawJob) -> None:
+def index_job(client: Hrflow, extracted_job: RawJob) -> None:
     job_already_indexed = client.job.indexing.get(
-        board_key=env_settings.BOARD_KEY, reference=raw_job.in_platform_id
+        board_key=env_settings.BOARD_KEY, reference=extracted_job.in_platform_id
     ).get("data")
 
     if not job_already_indexed:
-        job: HrflowJob = raw_job.api_format(client=client)
+        logger.info(f"job {extracted_job.in_platform_id} indexing ")
+        job: HrflowJob = extracted_job.api_format(client=client)
         client.job.indexing.add_json(board_key=env_settings.BOARD_KEY, job_json=asdict(job))
+        return
+
+    logger.info(
+        f"job {extracted_job.in_platform_id} already indexed, url : {extracted_job.full_url}"
+    )
